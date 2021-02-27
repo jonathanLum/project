d3d3aa6dee51192615ee19e7470a9ebfcffe9e00
jonathanLum <67062151+jonathanLum@users.noreply.github.com>
2021-02-26 22:08:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -33,36 +33,34 @@ var change;
 
 // Arrow buttons
 function plusSlides(n) {
-  clearTimeout(change);
-  showSlides(slideIndex += n);
+	clearTimeout(change);
+	showSlides(slideIndex += n);
 }
 // Dots
 function currentSlide(n) {
-  clearTimeout(change);
-  showSlides(slideIndex = n);
+	clearTimeout(change);
+	showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) 
-  {slideIndex = 1}
-  if (n < 1) 
-  {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  
-  change = setTimeout(function(){plusSlides(1)}, 2000);
+function showSlides(n) {// Learned and adapted from https://www.w3schools.com/
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) 
+	{slideIndex = 1}
+	if (n < 1) 
+	{slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	  
+	change = setTimeout(function(){plusSlides(1)}, 2000);
 }
-
-
 
 
 
