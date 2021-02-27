3668bf8c4c97799654b8bc8a9530a9bd2bbd4cc0
jonathanLum <67062151+jonathanLum@users.noreply.github.com>
2021-02-26 20:33:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,33 +4,6 @@ function bindButtons()
 {
 	document.getElementById('submit').addEventListener('click', function(event){
 		var req = new XMLHttpRequest();
-		var city = document.getElementById('city').value;
-		var zip = document.getElementById('zip').value;
-		var country = document.getElementById('country').value;
-        if(document.getElementById("useZip").checked)
-        {
-            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + country + "&appid=" + apiKey, true);
-        }  
-        else
-        {
-            req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + apiKey, true);
-        }
-		req.addEventListener('load',function()
-		{
-			if(req.status >= 200 && req.status < 400)
-			{
-				var response = JSON.parse(req.responseText);
-				console.log(response);
-				var msg = "City: " + response.name + "<br>" + "Country: " + response.sys.country + "<br>" + "Temp: " + response.main.temp + "<br>" + "Wind Speed: " + response.wind.speed + "<br>";
-				document.getElementById('form1Disp').innerHTML = msg;
-			}
-		});
-		req.send(null);
-		event.preventDefault();
-	});
-	
-	document.getElementById('postSubmit').addEventListener('click', function(event){
-		var req = new XMLHttpRequest();
 		var payload = {httpBin:document.getElementById('tosend').value};
 		req.open("POST", "http://httpbin.org/post", true);
 		req.setRequestHeader('Content-Type', 'application/json');
@@ -38,17 +11,16 @@ function bindButtons()
 		{
 			if(req.status >= 200 && req.status < 400)
 			{
-				var response = JSON.parse(req.responseText);
+				/*var response = JSON.parse(req.responseText);
 				console.log(response);
 				var response = JSON.parse(response.data);
-				var msg = "City: ";
-				document.getElementById('form2Disp').textContent = response.httpBin;
+				document.getElementById('response').textContent = response.httpBin;*/
+				document.getElementById('response').textContent = "Submitted!";
 			}
 		});
 		req.send(JSON.stringify(payload));
 		event.preventDefault();
 	});
-	
 }
 
 
