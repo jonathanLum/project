50c088b309d203737da0a814ca1b4b1987a23642
jonathanLum <67062151+jonathanLum@users.noreply.github.com>
2021-02-26 22:22:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -25,6 +25,27 @@ function bindButtons()
 }
 
 
+// CLOCK
+let c = document.querySelector(".clock");
+let d = document.querySelector(".date");
+
+function padding(n){
+	return n.toString().padStart(2, "0");
+}
+
+setInterval(() => {
+	let today = new Date();
+	let hh = today.getHours();
+	let mm = today.getMinutes();
+	let ss = today.getSeconds();
+	let date = today.toDateString();
+	
+	c.innerText = `${padding(hh)} : ${padding(mm)} : ${padding(ss)}`;
+	d.innerText = date;
+}, 1000);
+
+
+
 // SLIDESHOW
 // Vars
 var slideIndex = 1;
@@ -64,21 +85,4 @@ function showSlides(n) {
 
 
 
-// CLOCK
-let c = document.querySelector(".clock");
-let d = document.querySelector(".date");
 
-function padding(n){
-	return n.toString().padStart(2, "0");
-}
-
-setInterval(() => {
-	let today = new Date();
-	let hh = today.getHours();
-	let mm = today.getMinutes();
-	let ss = today.getSeconds();
-	let date = today.toDateString();
-	
-	c.innerText = `${padding(hh)} : ${padding(mm)} : ${padding(ss)}`;
-	d.innerText = date;
-}, 1000);
